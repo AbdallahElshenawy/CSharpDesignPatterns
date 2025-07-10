e8e1d51b5c6da6e26deddcd5b13ac9348a5d4363
Abdallah Elshenawy <elshenawya894@gmail.com>
2025-07-10 16:09:03 +0300

    Add Singleton Pattern

---
+++ Creational patterns/Singleton/RealWorldExampleLoadBalancer/Program.cs
@@ -0,0 +1,61 @@
+﻿public class Program
+{
+    /// <summary>
+    /// Singleton Design Pattern
+    /// </summary>
+    public static void Main()
+    {
+        var b1 = LoadBalancer.GetLoadBalancer();
+        var b2 = LoadBalancer.GetLoadBalancer();
+        var b3 = LoadBalancer.GetLoadBalancer();
+        var b4 = LoadBalancer.GetLoadBalancer();
+
+        if (b1 == b2 && b2 == b3 && b3 == b4)
+        {
+            Console.WriteLine("Same instance\n");
+        }
+
+        var balancer = LoadBalancer.GetLoadBalancer();
+       
+        for (int i = 0; i < 15; i++)
+        {
+            var server = balancer.NextServer.Name;
+            Console.WriteLine("Dispatch request to: " + server);
+        }
+        Console.ReadKey();
+    }
+}
+
+/// <summary>
+/// The 'Singleton' class
+/// </summary>
+public sealed class LoadBalancer
+{
+    // .NET guarantees thread safety for static initialization
+    private static readonly LoadBalancer instance = new();
+
+    private readonly List<Server> servers;
+    private readonly Random random = new();
+
+    private LoadBalancer()
+    {
+        servers = [
+                    new(Name: "ServerI", Ip: "120.14.220.18"),
+                    new(Name: "ServerII", Ip: "120.14.220.19" ),
+                    new(Name: "ServerIII", Ip: "120.14.220.20" ),
+                    new(Name: "ServerIV", Ip: "120.14.220.21" ),
+                    new(Name: "ServerV", Ip: "120.14.220.22" )
+                  ];
+    }
+
+    public static LoadBalancer GetLoadBalancer()
+    {
+        return instance;
+    }
+
+    public Server NextServer
+    {
+        get => servers[random.Next(servers.Count)];
+    }
+}
+public record Server(string Name, string Ip);
---
+++ Creational patterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var instance1 = Singleton.Instance;
+            var instance2 = Singleton.Instance;
+
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+            else
+            {
+                Console.WriteLine(" Objects are different instances");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object _lock = new();
+        private Singleton() { }
+   
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (_lock) // Ensure thread safety
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
